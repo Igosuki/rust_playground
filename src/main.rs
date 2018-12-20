e87a3e2786f61d4d331a430edbddbf9136219b94
Guillaume Balaine <igosuki@gmail.com>
2018-12-20 10:34:08 +0100

    Guess lower/higher random number instead

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,38 @@
 use std::io;
 use std::process::exit;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number (input your guess, enter 'e' to exit) !");
     let mut guess = String::new();
-
-    fn doguess(theguess: &mut String) {
-        match io::stdin().read_line(theguess) {
-            Ok(_) => {
-                theguess.pop();
-                match theguess.parse::<i64>() {
-                    Err(e) => {
-                        if theguess == "e" {
-                            return
-                        }
-                        println!("Invalid, please input a number, {}", e);
-                    },
-                    Ok(number) => if number % 2 == 0 {
-                        println!("You guessed wrong !");
-                    } else {
-                        println!("You guessed right !");
-                    }
-                }
-            },
-            Err(_) => println!("A system error happened, please retype !")
-        }
-        doguess(theguess);
-    }
-    doguess(&mut guess);
+    let magic_number = rand::thread_rng().gen_range(0, 101);
+    doguess(&mut guess, magic_number);
     exit(0);
+}
+
+fn doguess(theguess: &mut String, magic_number: i64) {
+    match io::stdin().read_line(theguess) {
+        Ok(_) => {
+            theguess.pop();
+            match theguess.parse::<i64>() {
+                Err(e) => {
+                    if theguess == "e" {
+                        return
+                    }
+                    println!("Invalid, please input a number, {}", e);
+                },
+                Ok(number) => if number == magic_number {
+                    println!("You guessed right !");
+                    return
+                } else if number > magic_number {
+                    println!("You guess is too high !");
+                } else {
+                    println!("You guess is too low !");
+                }
+            }
+        },
+        Err(_) => println!("A system error happened, please retype !")
+    }
+    theguess.clear();
+    doguess(theguess, magic_number);
 }
